2f38ba49da4828130cd0c1f111ee7c083c30e943
NaveenamKumar <naveenamkrun@gmail.com>
2019-09-12 16:31:38 +0530

    triying carousel 3 files modified

--- app/choose-template/choose-template.component.ts
+++ app/choose-template/choose-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-choose-template',
@@ -11,5 +12,26 @@ export class ChooseTemplateComponent implements OnInit {
 
   ngOnInit() {
   }
-
+ itemsPerSlide = 3;
+  singleSlideOffset = false;
+  noWrap = false;
+ 
+  slidesChangeMessage = '';
+ 
+  slides = [
+    {image: 'assets/images/nature/1.jpg'},
+    {image: 'assets/images/nature/2.jpg'},
+    {image: 'assets/images/nature/3.jpg'},
+    {image: 'assets/images/nature/4.jpg'},
+    {image: 'assets/images/nature/5.jpg'},
+    {image: 'assets/images/nature/6.jpg'},
+    {image: 'assets/images/nature/7.jpg'},
+    {image: 'assets/images/nature/8.jpg'},
+    {image: 'assets/images/nature/1.jpg'},
+    {image: 'assets/images/nature/2.jpg'}
+  ];
+ 
+  onSlideRangeChange(indexes: number[]): void {
+    this.slidesChangeMessage = `Slides have been switched: ${indexes}`;
+  }
 }
